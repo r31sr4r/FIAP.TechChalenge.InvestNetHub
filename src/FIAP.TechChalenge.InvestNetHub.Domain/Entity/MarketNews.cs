73aac46b1b29d63e39432dda739b41536b52b48c
Rafael Reis <rafael.reis.a@gmail.com>
2024-01-20 14:07:04 -0300

    feat: first commit

---
+++ src/FIAP.TechChalenge.InvestNetHub.Domain/Entity/MarketNews.cs
@@ -0,0 +1,78 @@
+﻿using FIAP.TechChalenge.InvestNetHub.Domain.Exceptions;
+
+namespace FIAP.TechChalenge.InvestNetHub.Domain.Entity
+{
+    public class MarketNews
+    {
+        public MarketNews(
+            string title,
+            string summary,
+            DateTime publishDate,
+            string url,
+            string source,
+            string imageUrl,
+            List<string> authors,
+            decimal overallSentimentScore,
+            string overallSentimentLabel
+        )
+        {
+            Id = Guid.NewGuid();
+            Title = title;
+            Summary = summary;
+            PublishDate = publishDate;
+            Url = url;
+            Source = source;
+            ImageUrl = imageUrl;
+            Authors = authors;
+            OverallSentimentScore = overallSentimentScore;
+            OverallSentimentLabel = overallSentimentLabel;
+
+            Validate();
+        }
+
+        public Guid Id { get; private set; }
+        public string Title { get; private set; }
+        public string Summary { get; private set; }
+        public DateTime PublishDate { get; private set; }
+        public string Url { get; private set; }
+        public string Source { get; private set; }
+        public string ImageUrl { get; private set; }
+        public List<string> Authors { get; private set; }
+        public decimal OverallSentimentScore { get; private set; }
+        public string OverallSentimentLabel { get; private set; }
+
+        public void Validate()
+        {
+            if (string.IsNullOrWhiteSpace(Title))
+                throw new EntityValidationException("Title cannot be empty or null.");
+
+            if (string.IsNullOrWhiteSpace(Summary))
+                throw new EntityValidationException("Summary cannot be empty or null.");
+
+            if (!ValidatePublishDate(PublishDate))            
+                throw new EntityValidationException("Publish date cannot be in the future.");
+
+            if (string.IsNullOrWhiteSpace(Url))
+                throw new EntityValidationException("URL cannot be empty or null.");
+
+            if (string.IsNullOrWhiteSpace(Source))
+                throw new EntityValidationException("Source cannot be empty or null.");
+
+            if (!ValidateAuthors(Authors))
+                throw new EntityValidationException("Authors list cannot be empty.");
+        }
+
+
+        private bool ValidatePublishDate(DateTime publishDate)
+        {
+            return publishDate <= DateTime.Now;
+        }
+
+        private bool ValidateAuthors(List<string> authors)
+        {
+            return authors != null && authors.Any();
+        }
+
+
+    }
+}
---
+++ tests/FIAP.TechChalenge.InvestNetHub.Tests/Domain/Entity/MarketNews/MarketNewsTest.cs
@@ -0,0 +1,188 @@
+﻿using FIAP.TechChalenge.InvestNetHub.Domain.Exceptions;
+using DomainEntity = FIAP.TechChalenge.InvestNetHub.Domain.Entity;
+
+namespace FIAP.TechChalenge.InvestNetHub.Tests.Domain.Entity.MarketNews;
+public class MarketNewsTest
+{
+    [Fact(DisplayName = nameof(Instantiate))]
+    [Trait("Domain", "MarketNews - Aggregates")]
+    public void Instantiate()
+    {
+        var validMarketNews = new
+        {
+            Title = "Test",
+            Summary = "Test Summary",
+            PublishDate = DateTime.Now.AddDays(-2),
+            Url = "http://www.teste.com.br",
+            Source = "Test Source",
+            ImageUrl = "http://www.imageurl.com",
+            Authors = new List<string> { "Author1", "Author2" },
+            OverallSentimentScore = 0.5m,
+            OverallSentimentLabel = "Neutral"
+        };
+
+        var marketNews = new DomainEntity.MarketNews(
+            validMarketNews.Title,
+            validMarketNews.Summary,
+            validMarketNews.PublishDate,
+            validMarketNews.Url,
+            validMarketNews.Source,
+            validMarketNews.ImageUrl,
+            validMarketNews.Authors,
+            validMarketNews.OverallSentimentScore,
+            validMarketNews.OverallSentimentLabel
+        );
+
+        Assert.NotNull(marketNews);
+        Assert.NotEqual(default(Guid), marketNews.Id);
+        Assert.Equal(validMarketNews.Title, marketNews.Title);
+        Assert.Equal(validMarketNews.Summary, marketNews.Summary);
+        Assert.Equal(validMarketNews.PublishDate, marketNews.PublishDate);
+        Assert.Equal(validMarketNews.Url, marketNews.Url);
+        Assert.Equal(validMarketNews.Source, marketNews.Source);
+        Assert.Equal(validMarketNews.ImageUrl, marketNews.ImageUrl);
+        Assert.Equal(validMarketNews.Authors, marketNews.Authors);
+        Assert.Equal(validMarketNews.OverallSentimentScore, marketNews.OverallSentimentScore);
+        Assert.Equal(validMarketNews.OverallSentimentLabel, marketNews.OverallSentimentLabel);
+
+    }
+
+    [Theory(DisplayName = nameof(Instantiate_InvalidTitle))]
+    [Trait("Domain", "MarketNews - Aggregates")]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData(" ")]
+    public void Instantiate_InvalidTitle(string? title)
+    {
+        Action action = () => new DomainEntity.MarketNews(
+            title!,
+            "Test Summary",
+            DateTime.Now.AddDays(-2),
+            "http://www.teste.com.br",
+            "Test Source",
+            "http://www.imageurl.com",
+            new List<string> { "Author1", "Author2" },
+            0.5m,
+            "Neutral");
+
+        var exception = Assert.Throws<EntityValidationException>(action);
+        Assert.Equal("Title cannot be empty or null.", exception.Message);
+    }
+
+    [Theory(DisplayName = nameof(Instantiate_InvalidSummary))]
+    [Trait("Domain", "MarketNews - Aggregates")]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData(" ")]
+    public void Instantiate_InvalidSummary(string? summary)
+    {
+        Action action = () => new DomainEntity.MarketNews(
+            "Test",
+            summary!,
+            DateTime.Now.AddDays(-2),
+            "http://www.teste.com.br",
+            "Test Source",
+            "http://www.imageurl.com",
+            new List<string> { "Author1", "Author2" },
+            0.5m,
+            "Neutral");
+
+        var exception = Assert.Throws<EntityValidationException>(action);
+        Assert.Equal("Summary cannot be empty or null.", exception.Message);
+    }
+
+    [Theory(DisplayName = nameof(Instantiate_InvalidUrl))]
+    [Trait("Domain", "MarketNews - Aggregates")]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData(" ")]
+    public void Instantiate_InvalidUrl(string? url)
+    {
+        Action action = () => new DomainEntity.MarketNews(
+            "Test",
+            "Test Summary",
+            DateTime.Now.AddDays(-2),
+            url!,
+            "Test Source",
+            "http://www.imageurl.com",
+            new List<string> { "Author1", "Author2" },
+            0.5m,
+            "Neutral");
+
+        var exception = Assert.Throws<EntityValidationException>(action);
+        Assert.Equal("URL cannot be empty or null.", exception.Message);
+    }
+
+    [Theory(DisplayName = nameof(Instantiate_InvalidSource))]
+    [Trait("Domain", "MarketNews - Aggregates")]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData(" ")]
+    public void Instantiate_InvalidSource(string? source)
+    {
+        Action action = () => new DomainEntity.MarketNews(
+            "Test",
+            "Test Summary",
+            DateTime.Now.AddDays(-2),
+            "http://www.teste.com.br",
+            source!,
+            "http://www.imageurl.com",
+            new List<string> { "Author1", "Author2" },
+            0.5m,
+            "Neutral");
+
+        var exception = Assert.Throws<EntityValidationException>(action);
+        Assert.Equal("Source cannot be empty or null.", exception.Message);
+    }
+
+    [Theory(DisplayName = nameof(Instantiate_InvalidPublishDate))]
+    [Trait("Domain", "MarketNews - Aggregates")]
+    [InlineData(1)] 
+    public void Instantiate_InvalidPublishDate(int daysInTheFuture)
+    {
+        var futureDate = DateTime.Now.AddDays(daysInTheFuture);
+
+        Action action = () => new DomainEntity.MarketNews(
+            "Test Title",
+            "Test Summary",
+            futureDate,
+            "http://www.teste.com.br",
+            "Test Source",
+            "http://www.imageurl.com",
+            new List<string> { "Author1", "Author2" },
+            0.5m,
+            "Neutral");
+
+        var exception = Assert.Throws<EntityValidationException>(action);
+        Assert.Equal("Publish date cannot be in the future.", exception.Message);
+    }
+
+    [Theory(DisplayName = nameof(Instantiate_InvalidAuthors))]
+    [Trait("Domain", "MarketNews - Aggregates")]
+    [MemberData(nameof(GetInvalidAuthors))]
+    public void Instantiate_InvalidAuthors(List<string> authors)
+    {
+        Action action = () => new DomainEntity.MarketNews(
+            "Test Title",
+            "Test Summary",
+            DateTime.Now.AddDays(-2),
+            "http://www.teste.com.br",
+            "Test Source",
+            "http://www.imageurl.com",
+            authors,
+            0.5m,
+            "Neutral");
+
+        var exception = Assert.Throws<EntityValidationException>(action);
+        Assert.Equal("Authors list cannot be empty.", exception.Message);
+    }
+
+    public static IEnumerable<object[]> GetInvalidAuthors()
+    {
+        yield return new object[] { new List<string>() };
+        yield return new object[] { null };
+    }
+
+
+
+}
