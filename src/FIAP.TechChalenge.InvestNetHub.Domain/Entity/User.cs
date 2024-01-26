511f312390d34adeaddbd6a41ef96134287650a9
Rafael Reis <rafael.reis.a@gmail.com>
2024-01-26 12:10:30 -0300

    feat: add user domain entity

---
+++ src/FIAP.TechChalenge.InvestNetHub.Domain/Entity/User.cs
@@ -0,0 +1,89 @@
+﻿using FIAP.TechChalenge.InvestNetHub.Domain.Exceptions;
+using FIAP.TechChalenge.InvestNetHub.Domain.SeedWork;
+using FIAP.TechChalenge.InvestNetHub.Domain.Validation;
+
+namespace Athenas.Pharma.Stock.Domain.Entity.PersonDomain;
+public class User : AggregateRoot 
+{
+    public User(
+        string name,
+        string email,
+        string phone,
+        string cpf,
+        DateTime dateOfBirth,
+        string password, 
+        string? rg = null,
+        bool isActive = true
+    ) : base() 
+    {
+        Name = name;
+        Email = email;
+        Phone = phone;
+        IsActive = isActive;
+        CreatedAt = DateTime.Now;
+        CPF = cpf;
+        RG = rg;
+        DateOfBirth = dateOfBirth;
+        Password = password;
+
+        Validate(); 
+    }
+
+    public string Name { get; private set; }
+    public string Email { get; private set; }
+    public string Phone { get; private set; }
+    public bool IsActive { get; private set; }
+    public DateTime CreatedAt { get; private set; }
+    public string CPF { get; private set; }
+    public string? RG { get; private set; }
+    public DateTime DateOfBirth { get; private set; }
+    public string Password { get; private set; }
+
+    public void Update(string name, string email, string phone, string cpf, DateTime dateOfBirth, string password, string? rg = null)
+    {
+        Name = name;
+        Email = email;
+        Phone = phone;
+        CPF = cpf;
+        RG = rg;
+        DateOfBirth = dateOfBirth;
+        Password = password; // Atualizando senha
+        Validate(); // Validação após atualização
+    }
+
+    // Método de validação que inclui todas as validações de Person e User
+    private void Validate()
+    {
+        if (string.IsNullOrWhiteSpace(Name))
+            throw new EntityValidationException($"{nameof(Name)} should not be empty or null");
+        if (Name.Length < 3)
+            throw new EntityValidationException($"{nameof(Name)} should be greater than 2 characters");
+        if (string.IsNullOrWhiteSpace(Email))
+            throw new EntityValidationException($"{nameof(Email)} should not be empty or null");
+        if (!ValidationHelper.IsValidEmail(Email))
+            throw new EntityValidationException($"{nameof(Email)} is not in a valid format");
+        if (string.IsNullOrWhiteSpace(Phone))
+            throw new EntityValidationException($"{nameof(Phone)} should not be empty or null");
+        if (!ValidationHelper.IsValidPhone(Phone))
+            throw new EntityValidationException($"{nameof(Phone)} is not in a valid format");
+        if (string.IsNullOrWhiteSpace(CPF))
+            throw new EntityValidationException($"{nameof(CPF)} should not be empty or null");
+        if (!ValidationHelper.IsCpfValid(CPF))
+            throw new EntityValidationException($"{nameof(CPF)} is not valid");
+        if (string.IsNullOrWhiteSpace(Password))
+            throw new EntityValidationException($"{nameof(Password)} should not be empty or null");
+        // Considerar adicionar validações de complexidade da senha
+    }
+
+    public void Activate()
+    {
+        IsActive = true;
+        Validate();
+    }
+
+    public void Deactivate()
+    {
+        IsActive = false;
+        Validate();
+    }
+}
---
+++ src/FIAP.TechChalenge.InvestNetHub.Domain/Validation/ValidationHelper.cs
@@ -0,0 +1,75 @@
+﻿using System.Text.RegularExpressions;
+
+namespace FIAP.TechChalenge.InvestNetHub.Domain.Validation;
+public class ValidationHelper
+{
+    public static bool IsCpfValid(string cpf)
+    {
+        if (string.IsNullOrWhiteSpace(cpf))
+            return false;
+
+        cpf = cpf.Trim().Replace(".", "").Replace("-", "");
+
+        if (cpf.Length != 11)
+            return false;
+
+        for (int j = 0; j < 10; j++)
+            if (j.ToString().PadLeft(11, char.Parse(j.ToString())) == cpf)
+                return false;
+
+        int[] multiplier1 = new int[] { 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+        int[] multiplier2 = new int[] { 11, 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+
+        string tempCpf = cpf.Substring(0, 9);
+        int sum = 0;
+
+        for (int i = 0; i < 9; i++)
+            sum += int.Parse(tempCpf[i].ToString()) * multiplier1[i];
+
+        int remainder = sum % 11;
+
+        if (remainder < 2)
+            remainder = 0;
+        else
+            remainder = 11 - remainder;
+
+        string digit = remainder.ToString();
+        tempCpf = tempCpf + digit;
+        sum = 0;
+
+        for (int i = 0; i < 10; i++)
+            sum += int.Parse(tempCpf[i].ToString()) * multiplier2[i];
+
+        remainder = sum % 11;
+
+        if (remainder < 2)
+            remainder = 0;
+        else
+            remainder = 11 - remainder;
+
+        digit = digit + remainder.ToString();
+
+        return cpf.EndsWith(digit);
+    }
+
+    public static bool IsValidEmail(string email)
+    {
+        try
+        {
+            var addr = new System.Net.Mail.MailAddress(email);
+            return addr.Address == email;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+
+    public static bool IsValidPhone(string phone)
+    {
+        var regex = new Regex(@"^\(\d{2}\) \d{4,5}-\d{4}$");
+        return regex.IsMatch(phone);
+    }
+
+}
+
