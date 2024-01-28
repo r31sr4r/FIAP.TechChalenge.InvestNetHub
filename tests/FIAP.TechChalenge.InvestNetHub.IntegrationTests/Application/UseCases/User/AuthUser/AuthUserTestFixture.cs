0a2a3b5c6ee34b55af89386dadc0d0f661a57f0a
Rafael Reis <rafael.reis.a@gmail.com>
2024-01-27 22:06:09 -0300

    test: auth user use case

---
+++ tests/FIAP.TechChalenge.InvestNetHub.IntegrationTests/Application/UseCases/User/AuthUser/AuthUserTestFixture.cs
@@ -0,0 +1,12 @@
+﻿using FIAP.TechChalenge.InvestNetHub.IntegrationTests.Application.UseCases.User.Common;
+
+namespace FIAP.TechChalenge.InvestNetHub.IntegrationTests.Application.UseCases.User.AuthUser;
+
+[CollectionDefinition(nameof(AuthUserTestFixture))]
+public class AuthUserTestFixtureCollection
+    : ICollectionFixture<AuthUserTestFixture>
+{ }
+
+public class AuthUserTestFixture
+    : UserUseCasesBaseFixture
+{ }
